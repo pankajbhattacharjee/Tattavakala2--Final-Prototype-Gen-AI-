0ab874b9028fd94c406404d281afdbbe1a44072f
Pankaj Bhattacharjee <pankajbhattacharjee634@gmail.com>
2025-10-28 03:29:07 +0000

    https://res.cloudinary.com/dpkhf4cf5/image/upload/v1761621812/bacground_

--- next.config.ts
+++ next.config.ts
@@ -45,6 +45,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.pinimg.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
