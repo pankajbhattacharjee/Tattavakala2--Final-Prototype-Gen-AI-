b7cdab30d273fc377fa96dce8cdc8a7dfc756c8b
Pankaj Bhattacharjee <pankajbhattacharjee634@gmail.com>
2025-11-01 23:34:32 +0000

    give the size ore the 1 gb because its not work

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     serverActions: {
-      bodySizeLimit: '4mb',
+      bodySizeLimit: '1gb',
     },
   },
   async rewrites() {
@@ -46,7 +46,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https://',
         hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
