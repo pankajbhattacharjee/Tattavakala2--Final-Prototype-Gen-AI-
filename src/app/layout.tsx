ff54175b91ac10a79888921fee8fa51cdc0ae941
Pankaj Bhattacharjee <pankajbhattacharjee634@gmail.com>
2025-11-01 18:04:21 +0000

    check the eror

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,9 +30,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-       <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-      </head>
       <body className={cn('font-sans antialiased', fontSans.variable, fontSerif.variable)}>
         <Providers>
             {children}
